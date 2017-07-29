548794c04f33cf9b4ab4ccbda285c50f11301259
Brennan Vincello <brennanv03@gmail.com>
2017-07-29 11:21:08 -0700

    refactor path logic, configs

--- lib/app.py
+++ lib/app.py
@@ -11,8 +11,6 @@ sys.path.pop(0)
 
 WORD_RE = re.compile(r'\w')
 ARGUMENT_RE = re.compile(r'[a-zA-Z0-9_=\*"\']+')
-
-# def __init__(self):
 default_sys_path = sys.path
 _input = io.open(sys.stdin.fileno(), encoding='utf-8')
 devnull = open(os.devnull, 'w')
@@ -32,8 +30,8 @@ def get_top_level_module(path):
     return path
 
 
-def set_request_config(config):
-    """Sets config values for current request.
+def set_paths(top_module_path, extra_paths):
+    """Sets path values for current request.
 
     This includes sys.path modifications which is getting restored to
     default value on each request so each project should be isolated
@@ -42,14 +40,14 @@ def set_request_config(config):
     Args:
         config: Dictionary with config values.
     """
+
     sys.path = default_sys_path
-    use_snippets = config.get('useSnippets')
-    show_doc_strings = config.get('showDescriptions', True)
-    fuzzy_matcher = config.get('fuzzyMatcher', False)
-    jedi.settings.case_insensitive_completion = config.get('caseInsensitiveCompletion', True)
-    for path in config.get('extraPaths', []):
+    for path in extra_paths:
         if path and path not in sys.path:
             sys.path.insert(0, path)
+    if top_module_path not in sys.path:
+        sys.path.insert(0, path)
+    pass
 
 
 def process_request(request):
@@ -58,33 +56,40 @@ def process_request(request):
     if not request:
         return
     request = json.loads(request)
-    set_request_config(request.get('config', {}))
+    config = request.get('config', {})
 
-    path = get_top_level_module(request.get('path', ''))
-    if path not in sys.path:
-        sys.path.insert(0, path)
+    path = request.get('path', '')
+    top_module_path = get_top_level_module(path)
+    extra_paths = config.get('extraPaths', [])
+    set_paths(top_module_path, extra_paths)
 
-    lookup = request.get('lookup', 'completions')
+    case_insensitive = config.get('caseInsensitiveCompletion', True)
+    jedi.settings.case_insensitive_completion = case_insensitive
 
     script = jedi.api.Script(source=request['source'], line=request['line'] + 1,
-                             column=request['column'], path=request.get('path', ''))
-    rid = request['id']
-    arguments = None
+                             column=request['column'], path=path)
+    arguments = []
+    lookup = request.get('lookup', 'completions')
+
     if lookup == 'definitions':
         results = get_definitions(script.goto_assignments())
     elif lookup == 'tooltip':
         results = get_tooltip(script.goto_assignments())
     elif lookup == 'arguments':
+        use_snippets = config.get('useSnippets')
+        arguments = get_arguments(script, use_snippets)
         results = []
-        arguments = get_arguments(script)
     elif lookup == 'usages':
         results = get_usages(script.usages())
     elif lookup == 'methods':
         results = get_methods(script)
     else:
-        results = get_completions(script, request.get('prefix', ''))
+        prefix = request.get('prefix', '')
+        fuzzy_matcher = config.get('fuzzyMatcher', False)
+        show_doc_strings = config.get('showDescriptions', True)
+        results = get_completions(script, prefix, fuzzy_matcher, show_doc_strings)
 
-    response = {'id': rid, 'results': results}
+    response = {'id': request['id'], 'results': results}
     if arguments:
         response['arguments'] = arguments
     return response
